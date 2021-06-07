b52711ba92eb2fd4a094c0ae2508902c798ff5a3
asadozzaman <asadozzaman278061@gmail.com>
2021-06-07 16:41:14 +0600

    some model create and migrate

---
+++ accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students
+
+# Register your models here.
+class UserModel(UserAdmin):
+    pass
+
+
+admin.site.register(CustomUser, UserModel)
+
+admin.site.register(AdminHOD)
+admin.site.register(Staffs)
+admin.site.register(Courses)
+admin.site.register(Subjects)
+admin.site.register(Students)
+
--- ourschool/settings.py
+++ ourschool/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#For Custom USER
+AUTH_USER_MODEL = "accounts.CustomUser"
